779b332c36a24ddbd2e5bac6bda2f5eaf8b8c906
0m3g4ki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-11-23 01:26:05 +0530

    Update and rename OmegaUtilityDriver.c to OmegaUtilityDriver.cpp

--- src/OmegaUtilityDriver.cpp
+++ src/OmegaUtilityDriver.cpp
@@ -1,12 +1,12 @@
 /**
- * @file OmegaUtilityDriver.c
+ * @file OmegaUtilityDriver.cpp
  * @author Omegaki113r
  * @date Tuesday, 2nd July 2024 1:01:18 pm
  * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
  * */
 /*
  * Project: OmegaUtilityDriver
- * File Name: OmegaUtilityDriver.c
+ * File Name: OmegaUtilityDriver.cpp
  * File Created: Tuesday, 2nd July 2024 1:01:18 pm
  * Author: Omegaki113r (omegaki113r@gmail.com)
  * -----
@@ -20,4 +20,4 @@
  * ----------	---	---------------------------------------------------------
  */
 
-#include "OmegaUtilityDriver.h"
+#include "OmegaUtilityDriver.hpp"
