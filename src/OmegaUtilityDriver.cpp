ad6753dc24cd0ded3073896f42a85cf19736cbd4
0m3g4ki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-12-17 13:49:46 +0530

    Update OmegaUtilityDriver.cpp

--- src/OmegaUtilityDriver.cpp
+++ src/OmegaUtilityDriver.cpp
@@ -21,11 +21,53 @@
  */
 
 #include <vector>
+#include <algorithm>
 #include "OmegaUtilityDriver.hpp"
 
-internal std::vector<u64> _s_handles;
+namespace Omega
+{
 
-OmegaHandle OmegaUtilityDriver_generate_handle(){
-    return ((u64)RAND() << 48) ^ ((u64)RAND() << 35) ^ ((u64)RAND() << 22) ^
-             ((u64)RAND() << 9) ^ ((u64)RAND() >> 4);
+constexpr internal u8 MAX_ITERATION_AMOUNT = 100;
+internal std::vector<OmegaHandle> _s_handles{};
+
+OmegaHandle OmegaUtilityDriver_generate_handle()
+{
+    u64 iteration_count = 0;
+    u64 generated_handle = 0;
+    do
+    {
+    regenerate_handle:
+        generated_handle = ((u64)RAND() << 48) ^ ((u64)RAND() << 35) ^ ((u64)RAND() << 22) ^
+                           ((u64)RAND() << 9) ^ ((u64)RAND() >> 4);
+        if(std::find(_s_handles.begin(), _s_handles.end(), generated_handle) != _s_handles.end())
+        {
+            goto regenerate_handle;
+        }
+    } while (generated_handle <= 0 && iteration_count++ < MAX_ITERATION_AMOUNT);
+    if (iteration_count >= MAX_ITERATION_AMOUNT)
+    {
+        OMEGA_LOGE("Unable to create valid handle");
+        generated_handle = 0;
+        goto response;
+    }
+    _s_handles.push_back(generated_handle);
+response:
+    OMEGA_LOGV("Generated Handle: %llu, Vector Size: %d", generated_handle, _s_handles.size());
+    return generated_handle;
+}
+
+bool OmegaHandleGenerator_delete_handle(OmegaHandle in_handle)
+{
+    bool ret = false;
+    if((auto iterator = std::find(_s_handles.begin(), _s_handles.end(), generated_handle)) != _s_handles.end())
+    {
+        _s_handles.erase(iterator);
+        ret = true;
+        goto response;
+    }
+response:
+    OMEGA_LOGV("Deleting Handle: %llu, Vector Size: %d", in_handle, _s_handles.size());
+    return ret;
+}
+
 }
