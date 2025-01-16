6f7b812fe0094977ca810586c9168aa2e24748cb
0m3g4ki113r <44817852+Omegaki113r@users.noreply.github.com>
2025-01-17 01:12:06 +0530

    Update OmegaUtilityDriver.hpp

--- inc/OmegaUtilityDriver.hpp
+++ inc/OmegaUtilityDriver.hpp
@@ -59,7 +59,8 @@
 #define PROFILE_TEXT_COLOR "38;5;207"
 #define END_PARAMETER "m"
 
-inline long map(long x, long in_min, long in_max, long out_min, long out_max) {
+template<typename T>
+inline T map(T x, T in_min, T in_max, T out_min, T out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
