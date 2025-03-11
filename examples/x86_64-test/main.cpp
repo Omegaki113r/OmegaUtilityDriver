3eb2572f66dec0819e2a798a0f1280f108b8efe1
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2025-03-11 19:30:51 +0530

    adding example to test x86 64

---
+++ examples/x86_64-test/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file main.cpp
+ * @author Omegaki113r
+ * @date Tuesday, 11th March 2025 7:12:20 pm
+ * @copyright Copyright 2024 - 2025 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaUtilityDriver
+ * File Name: main.cpp
+ * File Created: Tuesday, 11th March 2025 7:12:20 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Tuesday, 11th March 2025 7:14:38 pm
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2025 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#include <stdio.h>
+
+#include <OmegaUtilityDriver/UtilityDriver.hpp>
+
+int main()
+{
+    u16 number = 0;
+
+    return 0;
+}
